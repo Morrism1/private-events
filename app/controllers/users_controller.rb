3670616af91ba2a35dd048e4507f7d83732f38a4
Morrism1 <morrismontana0@gmail.com>
2020-10-01 16:16:15 +0200

    Create sign up option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Please confirm your email address to continue"
+      redirect_to events_path
+    else
+      flash[:error] = "Ooooppss, something went wrong!"
+      render 'new'
+    end
+  end
 
   def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
