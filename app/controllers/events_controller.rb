e418ae8b230ae6d582807553e8757bbe5884be54
Morrism1 <morrismontana0@gmail.com>
2020-10-05 17:16:21 +0200

    Creating events controllers' actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+
+    def new
+        @event = current_user.events.build
+    end
+
+    def create
+        @event = current_user.events.build
+        if @event.save 
+            flash[:sucess] = "Event Successfully created..."
+            redirect_to event_path(@event)
+        else
+            render "new"
+        end
+
+    end
+
+    def show 
+        @event = Event.find(params[:id])
+    end
 end
