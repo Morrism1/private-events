badc425b7b1f1e0738e07ab058bb13e0beaf4058
Morrism1 <morrismontana0@gmail.com>
2020-10-07 12:49:49 +0200

    Fix linter errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,19 @@
 class EventsController < ApplicationController
+  def new
+    @event = current_user.events.build
+  end
 
-    def new
-        @event = current_user.events.build
+  def create
+    @event = current_user.events.build
+    if @event.save
+      flash[:sucess] = 'Event Successfully created...'
+      redirect_to event_path(@event)
+    else
+      render 'new'
     end
+  end
 
-    def create
-        @event = current_user.events.build
-        if @event.save 
-            flash[:sucess] = "Event Successfully created..."
-            redirect_to event_path(@event)
-        else
-            render "new"
-        end
-
-    end
-
-    def show 
-        @event = Event.find(params[:id])
-    end
+  def show
+    @event = Event.find(params[:id])
+  end
 end
