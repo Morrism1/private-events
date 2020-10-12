5f7f6cb774318031b8fadfe7baa45e6825b4f899
Morrism1 <morrismontana0@gmail.com>
2020-10-12 17:20:18 +0200

    Add feature to deal with attending events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: 'event_attendees'
   has_many :event_attendees, foreign_key: 'attended_event_id'
   scope :upcoming, -> { where("Date >= ?", Date.today).order('Date ASC') }
-  scope :past, 	 -> { where("Date <  ?", Date.today).order('Date DESC') }
+  scope :past, -> { where("Date <  ?", Date.today).order('Date DESC') }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  scope :previous_events, -> { attended_events.where('date < ?', Date.today)}
-  # Ex:- scope :active, lambda {where(:active => true)}
   has_many :events, foreign_key: 'creator_id'
   has_many :event_attendees, foreign_key: 'attendee_id'
   has_many :attended_events, through: 'event_attendees'
@@ -9,5 +7,21 @@ class User < ApplicationRecord
   def upcoming_events
     self.attended_events.upcoming
   end
+
+  def previous_events
+		self.attended_events.past
+  end
   
+  def attending?(event)
+    event.attendees.include?(self)
+  end
+  
+  def attend!(event)
+    self.event_attendees.create!(attended_event_id: event.id)
+  end
+  
+  def cancel!(event)
+    self.event_attendees.find_by(attended_event_id: event.id).destroy
+  end
+
 end
