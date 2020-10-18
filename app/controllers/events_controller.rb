c7aad360e1c850b3d44926ec2732152670582c63
Morrism1 <morrismontana0@gmail.com>
2020-10-18 21:52:52 +0200

    Add before action in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+
+  before_action :logged_in_user, only: [ :new, :create, :edit, :update, :destroy]
+  
   def index
     @events_upcoming = Event.upcoming
     @events_past = Event.past
