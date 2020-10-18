af42a9c19a67d108160483af14f07db831e5aa58
Morrism1 <morrismontana0@gmail.com>
2020-10-18 22:46:20 +0200

    Fix linters errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,6 @@
 class EventsController < ApplicationController
+  before_action :logged_in_user, only: %i[new create edit update destroy]
 
-  before_action :logged_in_user, only: [ :new, :create, :edit, :update, :destroy]
-  
   def index
     @events_upcoming = Event.upcoming
     @events_past = Event.past
