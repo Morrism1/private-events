a053a30bd021ed729cfa6dd6275460505f54aa5f
Morrism1 <morrismontana0@gmail.com>
2020-10-20 16:13:01 +0200

    Remove logic from show page in events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def attend_button
+    if logged_in?
+      if @event.attendees.to_a.none?(current_user)
+        render 'attend_form'
+      else
+        content_tag(:p, 'You are already attending this event')
+      end
+    else
+      content_tag(:p, 'You must log in or sign up to view the attendees')
+    end
+  end
 end
